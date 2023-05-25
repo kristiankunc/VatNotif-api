85ad97bc6d237184fed3206bc7bb2a0ad9fa972e
Kristián Kunc <kristian.kunc@gmail.com>
2023-05-25 17:11:40 +0200

    change notification icon

--- src/vatsim.ts
+++ src/vatsim.ts
@@ -96,7 +96,7 @@ export class Vatsim {
 						},
 					],
 					username: "VatNotif",
-					avatar_url: "https://vatnotif.kristn.co.uk/favicon.png",
+					avatar_url: "https://vatnotif.kristn.co.uk/brand/logo.png",
 					attachments: [],
 				}),
 			});
