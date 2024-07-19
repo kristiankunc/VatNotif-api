96b803c9c436b478949c1f225d2dd666724a44fa
Kristián Kunc <kristian.kunc@gmail.com>
2024-07-19 13:44:29 +0200

    automatically delete old log files

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,4 +1,5 @@
 import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 export const logger = winston.createLogger({
 	level: "info",
@@ -9,10 +10,12 @@ export const logger = winston.createLogger({
 		winston.format.printf(({ level, message, timestamp }) => `${timestamp} ${level.toUpperCase()}: ${message}`)
 	),
 	transports: [
-		new winston.transports.File({
-			filename: "combined.log",
-			maxsize: 5242880, // 5MB
-			maxFiles: 5,
+		new DailyRotateFile({
+			filename: "combined-%DATE%.log",
+			datePattern: "YYYY-MM-DD",
+			zippedArchive: true,
+			maxSize: "5m",
+			maxFiles: "5d",
 		}),
 	],
 });
