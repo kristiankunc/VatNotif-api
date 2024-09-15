a36052bae0e2c51a483164cadcdea25e5ec2f37b
Kristián Kunc <kristian.kunc@gmail.com>
2024-09-15 15:59:23 +0200

    improve logging config

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,34 +1,38 @@
 import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 
-export const logger = winston.createLogger({
-	level: "info",
-	format: winston.format.combine(
-		winston.format.timestamp({
-			format: "YYYY-MM-DD HH:mm:ss",
-		}),
-		winston.format.printf(({ level, message, timestamp }) => `${timestamp} ${level.toUpperCase()}: ${message}`)
-	),
-	transports: [
+const transports = [];
+
+if (process.env.NODE_ENV === "production") {
+	transports.push(
 		new DailyRotateFile({
 			filename: "combined-%DATE%.log",
-			datePattern: "YYYY-MM-DD",
+			datePattern: "DD-MM-YYYY",
 			zippedArchive: true,
 			maxSize: "5m",
-			maxFiles: "5d",
-		}),
-	],
-});
-
-if (process.env.NODE_ENV !== "production") {
-	logger.add(
+			maxFiles: "7d",
+		})
+	);
+} else {
+	transports.push(
 		new winston.transports.Console({
 			format: winston.format.combine(
 				winston.format.timestamp({
-					format: "YYYY-MM-DD HH:mm:ss",
+					format: "DD-MM-YYYY HH:mm:ss",
 				}),
 				winston.format.printf(({ level, message, timestamp }) => `${timestamp} ${level.toUpperCase()}: ${message}`)
 			),
 		})
 	);
 }
+
+export const logger = winston.createLogger({
+	level: "info",
+	format: winston.format.combine(
+		winston.format.timestamp({
+			format: "DD-MM-YYYY HH:mm:ss",
+		}),
+		winston.format.printf(({ level, message, timestamp }) => `${timestamp} ${level.toUpperCase()}: ${message}`)
+	),
+	transports: transports,
+});
